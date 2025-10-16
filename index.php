10dc204b5d18679e933476f44bfe5d0ddd994e23
leonardoamichael <leonardoamichael@students.abtech.edu>
2025-10-15 21:26:08 -0400

    creating a homepage webhost

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>WEB-250 Projects</title>
+  <link href="css/styles.css" rel="stylesheet">
+  <script src="js/scripts.js" defer></script>
+
+  <!-- Font Awesome CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+
+<body>
+  <h1>WEB-250 PHP & MySQL Projects</h1>
+  <section id="github">
+    <a class="card" href="https://github.com/leonardoamichael/web250" target="blank">
+    <i class="fa-brands fa-github fa-5x"></i><br>
+    Link to GitHub
+    </a>
+  </section>
+  <section>
+    <h2>Assignment 4</h2>
+    <ul class="card-grid">
+      <li><a class="card" href="assignment04">Assignment04</a></li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Assignment 5</h2>
+    <ul class="card-grid">
+      <li>
+        <a class="card" href="assignment05/asgn05-bike/public/index.php">
+          <i class="fa-solid fa-bicycle fa-3x"></i><br>
+          Bicycles Project
+        </a>
+      </li>
+      <li>
+        <a class="card" href="assignment05/asgn05-bird/public/index.php">
+          <i class="fa-solid fa-crow fa-3x"></i><br>
+          Birds Project
+        </a>
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Assignment 6</h2>
+    <ul class="card-grid">
+      <li>
+        <a class="card" href="assignment06/asgn06-bike/public/index.php">
+          <i class="fa-solid fa-bicycle fa-3x"></i><br>
+          Bicycles Project
+        </a>
+      </li>
+      <li>
+        <a class="card" href="assignment06/asgn06-bird/public/index.php">
+          <i class="fa-solid fa-crow fa-3x"></i><br>
+          Birds Project
+        </a>
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Assignment 7</h2>
+    <ul class="card-grid">
+      <li>
+        <a class="card" href="assignment07/bike/public/index.php">
+          <i class="fa-solid fa-bicycle fa-3x"></i><br>
+          Bicycles Project
+        </a>
+      </li>
+      <li>
+        <a class="card" href="assignment07/bird/public/index.php">
+          <i class="fa-solid fa-crow fa-3x"></i><br>
+          Birds Project
+        </a>
+      </li>
+    </ul>
+  </section>
+</body>
+
+</html>
